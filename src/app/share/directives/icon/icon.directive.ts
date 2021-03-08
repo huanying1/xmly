24f6b4526637939b6d5ee8437629d79c2c1638b3
2331265465 <2331265465@qq.com>
2021-03-08 15:11:31 +0800

    icon组件

---
+++ src/app/share/directives/icon/icon.directive.ts
@@ -0,0 +1,25 @@
+import {Directive, ElementRef, HostBinding, Input, OnChanges, Renderer2, SimpleChanges} from '@angular/core';
+import {IconType} from "./types";
+
+@Directive({
+  selector: '[xmIcon]',
+  // host: {
+  //   '[class.iconfont]': 'true'
+  // }
+})
+export class IconDirective implements OnChanges {
+  @HostBinding('class.iconfont') readonly hostClass = true
+  @Input('xmIcon') type: IconType
+
+  constructor(private el: ElementRef, private rd2: Renderer2) {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const {type} = changes
+    if (type.previousValue) {
+      this.rd2.removeClass(this.el.nativeElement, 'icon-' + type.previousValue)
+    }
+    this.rd2.addClass(this.el.nativeElement, 'icon-' + this.type)
+  }
+
+}

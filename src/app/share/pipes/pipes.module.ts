ee3a0adf2798e4f47c413f75108dde4ce6856da9
2331265465 <2331265465@qq.com>
2021-03-08 15:53:37 +0800

    number-format

---
+++ src/app/share/pipes/pipes.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { NumberFormatPipe } from './number-format.pipe';
+
+
+
+@NgModule({
+  declarations: [NumberFormatPipe],
+  exports:[NumberFormatPipe]
+})
+
+export class PiPesModule { }

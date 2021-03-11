1556ebc5c43e9759358341927dcfe3502d36740d
2331265465 <2331265465@qq.com>
2021-03-11 16:09:03 +0800

    获取详情页数据

--- src/app/pages/album/album.component.ts
+++ src/app/pages/album/album.component.ts
@@ -1,5 +1,9 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {AlbumService, AlbumTrackArgs} from "../../services/apis/album.service";
+import {forkJoin} from "rxjs";
+import {AlbumInfo, Anchor, RelateAlbum, Track} from "../../services/apis/types";
+import {CategoryService} from "../../services/business/category.service";
 
 @Component({
   selector: 'xm-album',
@@ -8,12 +12,43 @@ import {ActivatedRoute} from "@angular/router";
   changeDetection:ChangeDetectionStrategy.OnPush
 })
 export class AlbumComponent implements OnInit {
-  value = 214124214
-  constructor(private route:ActivatedRoute) { }
+  albumInfo: AlbumInfo;
+  score: number;
+  anchor: Anchor;
+  relateAlbums: RelateAlbum[];
+  tracks: Track[] = [];
+  total = 0;
+  trackParams: AlbumTrackArgs = {
+    albumId: '',
+    sort: 1,
+    pageNum: 1,
+    pageSize: 30
+  };
+  constructor(
+    private route:ActivatedRoute,
+    private albumServe:AlbumService,
+    private categoryServe: CategoryService,
+    private cdr: ChangeDetectorRef
+    ) { }
 
   ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('albumId')
-    console.log(id)
+    this.trackParams.albumId = this.route.snapshot.paramMap.get('albumId')
+    forkJoin([
+      this.albumServe.album(this.trackParams.albumId),
+      this.albumServe.albumScore(this.trackParams.albumId),
+      this.albumServe.relateAlbums(this.trackParams.albumId)
+    ]).subscribe(([albumInfo,score,relateAlbum]) => {
+      this.albumInfo = {...albumInfo.mainInfo,albumId:albumInfo.albumId}
+      this.score = score
+      this.anchor = albumInfo.anchorInfo
+      this.tracks = albumInfo.tracksInfo.tracks
+      this.total = albumInfo.tracksInfo.trackTotalCount
+      this.relateAlbums = relateAlbum.slice(0,10)
+      this.categoryServe.setSubCategory([this.albumInfo.albumTitle])
+      console.log(this.albumInfo)
+      this.cdr.markForCheck()
+    })
+
   }
 
 }

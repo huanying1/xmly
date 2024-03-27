29cce7f6f24e8cbbf2c962d5a84beaa396f39df3
谢波 <xiebo121314@gmail.com>
2024-03-27 19:28:46 +0800

    修改环境变量

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   // baseUrl: 'http://39.108.140.70:3333'
   // baseUrl: 'http://localhost:3333'
-  baseUrl: 'https://xmly-api.api.vercel.app/auth'
+  baseUrl: 'https://xmly-api.api.vercel.app'
 
 };
